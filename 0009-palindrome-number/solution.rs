f539099e78c6d60642c6f73c1b56420e3713a077
BLACK <152079049+BLACK0X80@users.noreply.github.com>
2026-04-04 12:46:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 2.2 MB (53.99%)

---
+++ 0009-palindrome-number/solution.rs
@@ -0,0 +1,7 @@
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 { return false; }
+        let s = x.to_string();
+        s == s.chars().rev().collect::<String>()
+    }
+}
