1b1b9655785317e1645180fde343c60da49ef480
BLACK <152079049+BLACK0X80@users.noreply.github.com>
2026-04-04 15:15:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 3 MB (56.20%)

--- 0041-first-missing-positive/solution.rs
+++ 0041-first-missing-positive/solution.rs
@@ -1,20 +1,18 @@
 impl Solution {
     pub fn first_missing_positive(mut nums: Vec<i32>) -> i32 {
         let n = nums.len();
-
         for i in 0..n {
-            while nums[i] > 0 && nums[i] <= n as i32 && nums[(nums[i] - 1) as usize] != nums[i] {
+            while nums[i] > 0 && nums[i] <= n as i32 {
                 let j = (nums[i] - 1) as usize;
+                if nums[i] == nums[j] { break; }
                 nums.swap(i, j);
             }
         }
-
         for i in 0..n {
             if nums[i] != (i + 1) as i32 {
                 return (i + 1) as i32;
             }
         }
-
         (n + 1) as i32
     }
 }
