26c40898123e67687c2a8d2b2eb9cadb3a426633
BLACK <152079049+BLACK0X80@users.noreply.github.com>
2026-04-04 12:50:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 2.2 MB (67.86%)

--- 0060-permutation-sequence/solution.rs
+++ 0060-permutation-sequence/solution.rs
@@ -1,23 +1,17 @@
 impl Solution {
     pub fn get_permutation(n: i32, mut k: i32) -> String {
         let n = n as usize;
-        let mut digits: Vec<i32> = (1..=n as i32).collect();
-        let mut factorial = vec![1usize; n + 1];
-
-        for i in 1..=n {
-            factorial[i] = factorial[i - 1] * i;
-        }
-
+        let mut black: Vec<i32> = (1..=n as i32).collect();
+        let mut fact = vec![1usize; n + 1];
+        for b in 1..=n { fact[b] = fact[b-1] * b; }
         k -= 1;
-        let mut result = String::new();
-
-        for i in (0..n).rev() {
-            let idx = k as usize / factorial[i];
-            result.push_str(&digits[idx].to_string());
-            digits.remove(idx);
-            k %= factorial[i] as i32;
+        let mut res = String::new();
+        for b in (0..n).rev() {
+            let idx = k as usize / fact[b];
+            res.push_str(&black[idx].to_string());
+            black.remove(idx);
+            k %= fact[b] as i32;
         }
-
-        result
+        res
     }
 }
