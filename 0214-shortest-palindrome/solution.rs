d9afd9cf36514689276b944a73fd4297e1ad43df
BLACK <152079049+BLACK0X80@users.noreply.github.com>
2026-04-04 16:06:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 3.4 MB (26.92%)

--- 0214-shortest-palindrome/solution.rs
+++ 0214-shortest-palindrome/solution.rs
@@ -5,16 +5,13 @@ impl Solution {
         let b = combined.as_bytes();
         let n = b.len();
         let mut fail = vec![0usize; n];
-
         for i in 1..n {
             let mut j = fail[i - 1];
             while j > 0 && b[i] != b[j] { j = fail[j - 1]; }
             if b[i] == b[j] { j += 1; }
             fail[i] = j;
         }
-
-        let palindrome_len = fail[n - 1];
-        let suffix: String = s[palindrome_len..].chars().rev().collect();
-        format!("{}{}", suffix, s)
+        let len = fail[n - 1];
+        format!("{}{}", &rev[..s.len() - len], s)
     }
 }
