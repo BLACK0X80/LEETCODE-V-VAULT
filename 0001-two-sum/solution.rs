89dc02a0946e4d6f2cfb0ee64ef0de1697680ee3
BLACK <152079049+BLACK0X80@users.noreply.github.com>
2026-04-04 15:13:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 2.6 MB (19.60%)

--- 0001-two-sum/solution.rs
+++ 0001-two-sum/solution.rs
@@ -2,13 +2,14 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map = HashMap::with_capacity(nums.len());
+        let mut m = HashMap::new();
         for (i, &n) in nums.iter().enumerate() {
-            if let Some(&j) = map.get(&(target - n)) {
+            let c = target - n;
+            if let Some(&j) = m.get(&c) {
                 return vec![j as i32, i as i32];
             }
-            map.insert(n, i);
+            m.insert(n, i);
         }
-        unreachable!()
+        vec![]
     }
 }
