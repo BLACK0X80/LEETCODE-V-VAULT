3ef61a4d7425f8e0ad0d0ab26b7d2bd09193e880
BLACK <152079049+BLACK0X80@users.noreply.github.com>
2026-04-09 16:35:57 +0000

    Sync LeetCode submission Runtime - 2 ms (41.20%), Memory - 2.9 MB (97.42%)

--- 0056-merge-intervals/solution.rs
+++ 0056-merge-intervals/solution.rs
@@ -2,16 +2,11 @@ impl Solution {
     pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         intervals.sort_unstable_by_key(|v| v[0]);
         let mut res: Vec<Vec<i32>> = Vec::new();
-        for interval in intervals {
+        for iv in intervals {
             if let Some(last) = res.last_mut() {
-                if last[1] >= interval[0] {
-                    last[1] = last[1].max(interval[1]);
-                } else {
-                    res.push(interval);
-                }
-            } else {
-                res.push(interval);
+                if last[1] >= iv[0] { last[1] = last[1].max(iv[1]); continue; }
             }
+            res.push(iv);
         }
         res
     }
