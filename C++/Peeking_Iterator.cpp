e306db3b37ddabf115bb3872abf6e580a0c32a06
BLACK <152079049+BLACK0X80@users.noreply.github.com>
2026-04-15 00:50:24 +0200

    Delete C++ directory

--- C++/Peeking_Iterator.cpp
+++
@@ -1,8 +0,0 @@
-class PeekingIterator : public Iterator {
-    int black_v; bool black_has;
-public:
-    PeekingIterator(const vector<int>& nums) : Iterator(nums) { black_has = Iterator::hasNext(); if (black_has) black_v = Iterator::next(); }
-    int peek() { return black_v; }
-    int next() { int res = black_v; black_has = Iterator::hasNext(); if (black_has) black_v = Iterator::next(); return res; }
-    bool hasNext() const { return black_has; }
-};
